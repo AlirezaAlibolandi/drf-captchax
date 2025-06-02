a61afd583f515df566812ddcc0712b3a53c94eb1
Alireza.A <alirezaalibolandi@duck.com>
2025-06-02 11:25:47 +0330

    feat(captchax): Add initial implementation of drf-captchax

---
+++ tests/settings.py
@@ -0,0 +1,45 @@
+"""
+Django settings for running tests.
+"""
+
+SECRET_KEY = 'test-key'
+
+INSTALLED_APPS = [
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'rest_framework',
+    'captchax',
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+]
+
+CAPTCHAX = {
+    'LENGTH': 6,
+    'WIDTH': 200,
+    'HEIGHT': 60,
+    'FONT_SIZE': 36,
+    'BACKGROUND_COLOR': '#ffffff',
+    'TEXT_COLOR': '#000000',
+    'NOISE_LEVEL': 20,
+    'USE_LINES': True,
+    'USE_DOTS': True,
+    'TIMEOUT': 300,
+    'CASE_SENSITIVE': False,
+    'MAX_ATTEMPTS': 5,
+    'BACKEND': 'captchax.backend.memory.MemoryBackend',
+}
